04e7d43a23f47bc617813bd80ae8fa270bb27284
khalilaliev <khalilaliev0@gmail.com>
2024-11-30 14:51:58 +0100

    complete-hw6

---
+++ App/Controllers/Error.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Controllers;
+
+class Error 
+{
+  public function index()
+  {
+    echo 'This is Error page';
+  }
+}
---
+++ App/Controllers/Gallery.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controllers;
+
+class Gallery 
+{
+  public function index()
+  {
+    echo 'This is Gallery page';
+  }
+  public function create()
+  {
+    echo 'This is Gallery page and method Create';
+  }
+  public function update()
+  {
+    echo 'This is Gallery page and method Update';
+  }
+  public function delete()
+  {
+    echo 'This is Gallery page and method Delete';
+  }
+}
